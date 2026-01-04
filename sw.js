7e7f54f6b725816c4150b0f2a26b4d29d5a6c194
waseemasghar7072-web <waseemasghar7072@gmail.com>
2026-01-05 01:41:05 +0530

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,4 @@
+const cacheName = 'hub-v1';
+const assets = ['index.html', 'candy.html', 'manifest.json'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))); });
